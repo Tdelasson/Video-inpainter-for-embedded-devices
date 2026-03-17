bf80cda61ac9a47fe7e81c23114f72b57892b50b
astridhb <ah23@student.aau.dk>
2026-03-17 12:50:14 +0100

    tilføjet en app fil

---
+++ nvidia_jetson/app.py
@@ -0,0 +1,11 @@
+import cv2
+
+
+
+#først find ud af hvad fil format man får fra jetson.
+# - AI fil output -> 
+# - kamera fil output ->  
+
+# hvordan henter man fra disk
+# hvordan hentes præcis disse fil-typer
+# kod det
